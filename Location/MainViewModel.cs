af16872d70f0982e0b7945570375fc16b638960c
Nazim Hajidin <nazhajidin@gmail.com>
2014-09-29 19:57:50 -0400

    Add view model

---
+++ Location/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+
+namespace Location
+{
+	public class MainViewModel : ReactiveObject
+	{
+		public MainViewModel()
+		{
+			Scan = ReactiveCommand.CreateAsyncObservable(
+				_ => Observable.Start(() =>
+					{
+						var list = new List<string>();
+
+						return list;
+                    }, RxApp.TaskpoolScheduler));
+
+			Scan.Subscribe(list =>
+				{
+					CellTowers.Clear();
+					CellTowers.AddRange(list);
+				});
+
+			CellTowers = new ReactiveList<string>();
+		}
+
+		public ReactiveCommand<List<string>> Scan  { get; private set; }
+
+		public ReactiveList<string> CellTowers { get; private set; }
+	}
+}
